0c2abfd599deafec368d06e6fa5ba3ba9ef1e752
Mark Lemay <riteofwhey@gmail.com>
2014-08-16 21:12:40 +0100

    whatever

--- massdebate/urls.py
+++ massdebate/urls.py
@@ -9,6 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'', include('debate.urls', namespace="debate")),
 )
